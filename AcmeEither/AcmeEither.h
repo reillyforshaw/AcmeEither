40216fe4c9288aa571b59e5fd24c7244eaf17539
Reilly Forshaw <reilly.forshaw@gmail.com>
2018-04-19 07:43:22 -0700

    Initial Commit

---
+++ AcmeEither/AcmeEither.h
@@ -0,0 +1,19 @@
+//
+//  AcmeEither.h
+//  AcmeEither
+//
+//  Created by Reilly Forshaw on 2018-04-19.
+//  Copyright © 2018 Reilly Forshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AcmeEither.
+FOUNDATION_EXPORT double AcmeEitherVersionNumber;
+
+//! Project version string for AcmeEither.
+FOUNDATION_EXPORT const unsigned char AcmeEitherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AcmeEither/PublicHeader.h>
+
+
